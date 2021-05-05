1ddba68e2eb9094836791f2ed85a67400e80708c
Maciej Zielonka <maciej.zielonka@yale.edu>
2021-05-05 12:14:21 -0400

    added convertSecs to helpers.py

--- helpers.py
+++ helpers.py
@@ -25,3 +25,28 @@ def get_files(directory, valid_exts):
 
 def generate_file_name(outDir, inFile, lowcut, highcut, suffix = "_filtered", extension = ".wav"):
     return outDir + remove_extension(path_leaf(inFile)) + '_' + str(int(lowcut)) + '-' + str(int(highcut)) + suffix + extension
+
+def convertMillis(millis):
+    """
+    Function to convert milliseconds into `MIN:SEC` Format
+    """
+    millis = int(millis)
+    seconds=(millis/1000)%60
+    seconds = int(seconds)
+    minutes=(millis/(1000*60))%60
+    minutes = int(minutes)
+    hours=(millis/(1000*60*60))%24
+    return ("%d:%d" % (minutes, seconds))
+
+def convertSecs(seconds, half_seconds=False):
+    """
+    function to convert seconds into `MIN:SEC` format
+    """
+    if half_seconds:
+        seconds /= 2.0
+    minutes=(seconds//60)%60
+    minutes = int(minutes)
+    frac, whole = math.modf(seconds)
+    seconds = whole % 60
+    seconds += frac
+    return ("%d:%.1f" % (minutes, seconds))
